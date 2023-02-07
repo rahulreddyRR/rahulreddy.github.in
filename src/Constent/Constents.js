4850ebadd108ff55c8633dc36d9495c04369fe2d
Rahul <raguly55@gmail.com>
2023-02-08 00:11:45 +0530

    updated constent data

--- src/Constent/Constents.js
+++ src/Constent/Constents.js
@@ -1,15 +1,11 @@
 export const NAV_ITEMS = [
   {
     label: "Home",
-    page: "home",
-  },
-  {
-    label: "About",
-    page: "about",
+    page: "#",
   },
   {
     label: "Projects",
-    page: "projects",
+    page: "#",
   },
 ];
 
@@ -31,5 +27,6 @@ export const Current_Learning_skills = [
   { skill: "React Native" },
 ];
 
-export const My_Github_url = "";
-export const My_Linked_url = "";
+export const My_Github_url = "https://github.com/rahulreddyRR";
+export const My_Linked_url =
+  "https://www.linkedin.com/in/rahul-reddy-y-001501200/";
